1b03113f4129730966c3b7e00caccb2da8919c2e
Maurice <mauricewillis44@gmail.com>
2023-05-31 00:41:11 +0800

    fix: Move firebaseConfig variables to private environment variables

--- firebase.js
+++ firebase.js
@@ -1,23 +1,23 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApp, getApps } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
-import { getStorage } from 'firebase/storage'
+import { initializeApp, getApp, getApps } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAG0anGh_KTHYL5483LceG412RoSllXv9s',
-  authDomain: 'cryptogram-9a102.firebaseapp.com',
-  projectId: 'cryptogram-9a102',
-  storageBucket: 'cryptogram-9a102.appspot.com',
-  messagingSenderId: '857896906285',
-  appId: '1:857896906285:web:20eb9a8a59474375f8d2a4',
-}
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
-const db = getFirestore()
-const storage = getStorage()
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const storage = getStorage();
 
-export { app, db, storage }
+export { app, db, storage };
