34812a2e26bbfe8f7f022aa99c41919fd772d6d0
Maurice <mauricewillis44@gmail.com>
2023-05-31 00:45:33 +0800

    fix: messagingSenderId typo

--- firebase.js
+++ firebase.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
 };
 
